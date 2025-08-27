c749136055942e76c4aa2d69b1354258c8abf05f
wendy-commits <wendy@perfectlyimperfect.org.nz>
2025-08-27 19:12:31 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize your Smoothie :cup_with_straw: ")
